0343fbcc63fbff8e040342c38ee65dfe3cee933d
Manvendra Singh <mzealpro@gmail.com>
2020-10-09 07:47:14 +0530

    Basketball Positions

---
+++ basketball.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+	printf("Which position would you like to play?\n");
+	printf("\t\t\t1.Point Guard\n");
+	printf("\t\t\t2.Shooting Guard\n");
+	printf("\t\t\t3.Center");
+	return 0;
+}
